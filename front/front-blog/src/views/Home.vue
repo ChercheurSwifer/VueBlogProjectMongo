c26810ee3cad0cceca01c7b3746784d2d34af31f
DavidSaniez <david.saniez.pro@gmail.com>
2022-01-27 12:58:24 +0100

    Article creation

--- front/front-blog/src/views/Home.vue
+++ front/front-blog/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div>
 
+      <div class="text-center">
+        <h1>Blog</h1>
+       <p> Bienvenue sur notre blog</p>
+
+       <div v-if="posts.length === 0">
+            <h2> Il n'y a pas encore d'article, revenez prochainement :) </h2>
+        </div>
+      </div>
+
+        <div class="row">
+           <div class="col-md-4" v-for="post in posts" :key="post._id">
+              <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                   <h2 class="card-img-top">{{ post.title }}</h2>
+                  <p class="card-text">{{ post.body }}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group" style="margin-bottom: 20px;">
+                      <router-link :to="{name: 'Post', params: {id: post._id}}" class="btn btn-sm btn-outline-secondary">Voir l'article </router-link>
+                       <router-link :to="{name: 'Edit', params: {id: post._id}}" class="btn btn-sm btn-outline-secondary">Editer l'article </router-link>
+                       <button class="btn btn-sm btn-outline-secondary" v-on:click="deletePost(post._id)">Supprimer l'article</button>
+                    </div>
+                  </div>
+
+                  <div class="card-footer">
+                    <small class="text-muted">Posté le: </small><br/>
+                    <small class="text-muted">par:</small>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+      </div>
+    </div>
+</template>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { server } from "@/utils/helper";
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      axios
+        .get(`${server.baseURL}/blog/posts`)
+        .then(data => (this.posts = data.data));
+    },
+    deletePost(id) {
+      axios.delete(`${server.baseURL}/blog/delete?postID=${id}`).then(data => {
+        console.log(data);
+        window.location.reload();
+      });
+    }
   }
-}
+};
 </script>
